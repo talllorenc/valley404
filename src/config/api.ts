9323f215d312f3fb5219af95377e9445a17c153b
talllorenc <talllorenc@gmail.com>
2024-09-09 21:25:01 +0500

    Debag Axios

--- src/config/api.ts
+++ src/config/api.ts
@@ -7,4 +7,15 @@ const options = {
 
 const API = axios.create(options);
 
+API.interceptors.response.use(
+	(response) => {
+		return response.data;
+	},
+	async (error) => {
+		if (error.response) {
+		}
+		return Promise.reject(error);
+	},
+);
+
 export default API;
